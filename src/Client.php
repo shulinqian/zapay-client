bce73923a5a614e3d9dbcc529b308032b4b9c725
qianqiangqiang <330576744@qq.com>
2017-02-20 11:32:54 +0800

    update client

--- src/Client.php
+++ src/Client.php
@@ -8,8 +8,65 @@ namespace thinkweb\zapay;
 
 class Client{
 
-    public function test(){
-        echo 111;
+    public function __construct($config = [])
+    {
+        if($config){
+            $this->setConfig($config);
+        }
     }
 
+    protected $config = [];
+
+    /**
+     * @return mixed
+     */
+    public function getConfig($key = null)
+    {
+        if($key === null){
+            return $this->config;
+        }
+        return isset($this->config[$key]) ? $this->config[$key] : null;
+    }
+
+    /**
+     * @param mixed $config
+     */
+    public function setConfig($config)
+    {
+        $this->config = $config;
+    }
+
+    public function bind($post){
+        $sign = $this->gav($post, 'sign');
+        $return_str = $this->gav($post, 'return_str');
+        if(!$sign || !$return_str){
+            return false;
+        }
+        return $this->auth($sign, $post);
+    }
+
+    protected function auth($sign, $data)
+    {
+        unset($data['sign']);
+        if(is_array($data)){
+            ksort($data);
+            $data = json_encode($data, JSON_NUMERIC_CHECK);
+        }
+        $appid = $this->getConfig('appid');
+        $key = $this->getConfig('key');
+        return $sign === md5($data . $appid . $key);
+    }
+
+    public function createLink($post)
+    {
+
+    }
+
+    public function notify(){
+
+    }
+
+    protected function gav($post, $key, $def = null){
+        return isset($post[$key]) ? $post[$key] : $def;
+    }
 }
